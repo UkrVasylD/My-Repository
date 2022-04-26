2ecbcd5d2f6b7ad89bc82ad2b3710ad1289a4070
Vasyl Drozd <0660343546v@gmail.com>
2022-04-26 16:37:15 +0300

    add emit to button product cart

--- my_first_online_shop/src/components/ProductList/ProductCart/index.vue
+++ my_first_online_shop/src/components/ProductList/ProductCart/index.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>
-    <div class="imeges">
-      <img src="imgSrc" />
+  <div class="product_cart">
+    <div>
+      <img
+        class="product_cart_imeges"
+        :src="require('../../../assets/img/' + product_data.img)"
+      />
+    </div>
+    <div class="product_cart_name">{{ product_data.Name }}</div>
+    <div class="product_cart_price">
+      Ціна товару {{ product_data.Price }} грн
     </div>
     <div>
-      Ціна товару {{ productPrice }} грн Наявність товару {{ productCount }} шт
-    </div>
-    <div class="discout">{{ productDiscount }}</div>
-    <div>
-      <button>Додати в кошик</button>
+      <button class="add_to cart_btn btn" @click="sendDataToParent">
+        Додати в кошик
+      </button>
     </div>
   </div>
 </template>
@@ -17,24 +22,46 @@
 export default {
   name: "ProductCart",
   props: {
-    imgSrc: {
-      type: String,
-      default: null,
+    product_data: {
+      type: Object,
+      default() {
+        return {};
+      },
     },
-    productCount: {
-      type: Number,
-      default: null,
-    },
-    productPrice: {
-      type: Number,
-      default: null,
-    },
-    productDiscount: {
-      type: Number,
-      default: null,
+    // imgSrc: {
+    //   type: String,
+    //   default: null,
+    // },
+    // productName: {
+    //   type: String,
+    //   default: "No name",
+    // },
+    // productPrice: {
+    //   type: Number,
+    //   default: null,
+    // },
+    // available: {
+    //   type: Number,
+    //   default: null,
+    // },
+  },
+  methods: {
+    sendDataToParent() {
+      this.emit("sendId", this.product_data.id);
     },
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.product_cart {
+  flex-basis: 25%;
+  box-shadow: 0 0 8px 0 black;
+  padding: 16px;
+  margin-bottom: 16px;
+}
+
+.product_cart_imeges {
+  max-width: 150px;
+}
+</style>
--- my_first_online_shop/src/components/ProductList/index.vue
+++ my_first_online_shop/src/components/ProductList/index.vue
@@ -1,13 +1,11 @@
 <template>
-  <div>
-    <div v-for="product in productArr" :key="product.id">
-      <product-cart
-        :imgSrc="product.imgSrc"
-        :productCount="product.productCount"
-        :productPrice="product.productPrice"
-        :productDiscount="product.productDiscount"
-      />
-    </div>
+  <div class="product-list-wraper">
+    <product-cart
+      v-for="product in productArr"
+      :key="product.id"
+      @sendId="showGoodsIdToConsole"
+      :product_data="product"
+    />
   </div>
 </template>
 
@@ -17,30 +15,72 @@ import ProductCart from "./ProductCart";
 export default {
   name: "ProductList",
 
-  components: {
-    ProductCart,
-  },
   data() {
     return {
-      id: 1,
       productArr: [
         {
-          imgSrc: "",
-          productCount: 1,
-          productPrice: 100,
-          productDiscount: 10,
+          id: "1",
+          img: "4039.jpg",
+          Name: "Goods 1",
+          Price: 200,
+          available: true,
         },
         {
-          id: 2,
-          imgSrc: "",
-          productCount: 1,
-          productPrice: 150,
-          productDiscount: 10,
+          id: "2",
+          img: "Bez_imeni_1.jpg",
+          Name: "Goods 2",
+          Price: 150,
+          available: true,
+        },
+        {
+          id: "3",
+          img: "bloki_striotelinie.png",
+          Name: "Goods 3",
+          Price: 300,
+          available: true,
+        },
+        {
+          id: "4",
+          img: "kirpich.png",
+          Name: "Goods 2",
+          Price: 400,
+          available: true,
+        },
+        {
+          id: "5",
+          img: "stroitelinaia_himia.png",
+          Name: "Goods 2",
+          Price: 500,
+          available: true,
+        },
+        {
+          id: "6",
+          img: "stroitelinaia_setka_i_plenka.png",
+          Name: "Goods 6",
+          Price: 600,
+          available: true,
         },
       ],
     };
   },
+  components: {
+    ProductCart,
+  },
+  methods: {
+    showGoodsIdToConsole(id) {
+      console.log(id);
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="css" scoped>
+.product-list-wraper {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 900px;
+  margin: 0 auto;
+}
+</style>
