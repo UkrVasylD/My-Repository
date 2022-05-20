fc26061ea5ef9af3507497f925a88497cb3715b4
Vasyl Drozd <0660343546v@gmail.com>
2022-05-20 10:29:50 +0300

    added my-beta-app bay vuetify

---
+++ my-beta-app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-main>
+      <HelloWorld />
+      <mySlider />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import mySlider from "./components/myComponent/mySlider.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    HelloWorld,
+    mySlider,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
